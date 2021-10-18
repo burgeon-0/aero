e0b6b87f8e008a31b659af038a42ada8cbd44096
Sam Lu <luxiaocong524@163.com>
2021-10-18 20:16:39 +0800

    Add spring-boot-oauth2-client

---
+++ user-service/src/main/java/org/burgeon/aero/us/Application.java
@@ -0,0 +1,17 @@
+package org.burgeon.aero.us;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sam Lu
+ * @date 2021/10/14
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ user-service/src/main/java/org/burgeon/aero/us/adapter/controller/UserController.java
@@ -0,0 +1,31 @@
+package org.burgeon.aero.us.adapter.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * @author Sam Lu
+ * @date 2021/10/16
+ */
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+    @GetMapping("/error")
+    public String error(HttpServletRequest request) {
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
+    }
+
+}
