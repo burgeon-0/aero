697c6b35ff34cdcdca6cc9b04b9b4f458e04251b
Sam Lu <luxiaocong524@163.com>
2021-11-03 19:16:34 +0800

    feat: Add Spring Security Config

--- auth-service/README.md
+++ auth-service/README.md
@@ -1,3 +1,5 @@
 # 参考资料
 
 1. [Serve Static Resources with Spring](https://www.baeldung.com/spring-mvc-static-resources)
+2. [Spring Security Form Login](https://www.baeldung.com/spring-security-login)
+3. [Custom Login Form in Spring Security](https://springhow.com/custom-form-login-in-spring-security/)
---
+++ auth-service/src/main/java/org/burgeon/aero/us/adapter/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package org.burgeon.aero.us.adapter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author Sam Lu
+ * @date 2021/11/3
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordEncoder().encode("aero"))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
