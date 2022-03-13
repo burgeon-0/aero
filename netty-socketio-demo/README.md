324c81ade7fbe37b634ec07dab2bb9c169191b4e
Sam Lu <luxiaocong524@163.com>
2022-03-13 21:02:31 +0800

    feat(1.0.0): 增加netty-socketio-demo

---
+++ netty-socketio-demo/README.md
@@ -0,0 +1,4 @@
+# Demo project for Netty socketio
+
+参考资料：
+- [Netty-socketio Demo](https://github.com/mrniko/netty-socketio-demo)
---
+++ netty-socketio-demo/src/main/java/org/burgeon/aero/nsd/Message.java
@@ -0,0 +1,18 @@
+package org.burgeon.aero.nsd;
+
+import lombok.Data;
+
+/**
+ * @author Sam Lu
+ * @date 2022/3/13
+ */
+@Data
+public class Message {
+
+    private String color;
+
+    private String userName;
+
+    private String message;
+
+}
